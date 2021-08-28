fdec6c86dc0522cab0fb4ced05148a16331890fa
Oluwatamilore Aladesuyi <89304201+Talade@users.noreply.github.com>
2021-08-28 08:02:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement)
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
-    this.clear()
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equals = document.getElementsByClassName("equals")[0];
+const display = document.getElementById("display");
+const clear = document.getElementById("clear");
+const history = document.getElementById("history");
+const backspace = document.getElementById("backspace");
+
+number.forEach((btn) => btn.addEventListener("click", addNumber));
+
+function addNumber(e) {
+  e.preventDefault();
+
+  history.innerText += e.target.id;
+  display.innerText += e.target.id;
 }
 
-clear() {
-    this.currentOperand = " "
-    this.previousOperand = " "
-    this.operator = undefined
-    }
-    delete() {
-    
-    }
-    appendNumber(number) {
-    this.currentOperand = number
-    }
-    chooseOperator(operator) {
-    
-    }
-    compute() {
-    
-    }
-    updateDisplay() {
-    this.currentOperandTextElement = this.currentOperand
-    }
+operator.forEach((btn) => btn.addEventListener("click", addOperator));
 
-const numberButtons = document.querySelectorAll("[number]")
-const operatorButtons = document.querySelectorAll("[operator]")
-const clearButton = document.querySelector("[clear]")
-const deleteButton = document.querySelector("[delete]")
-const equalButton = document.querySelector("[equal]")
-const previousOperandTextElement = document.querySelector("[previous-operand]")
-const currentOperandTextElement = document.querySelector("[current-operand]")
+function addOperator(e) {
+  e.preventDefault();
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-numberButtons.forEach(button => {
-    button.addEventListener("click", () => {
-calculator.appendNumber(button.innerText)
-calculator.updateDisplay()
-    })
-})
+  history.innerText += e.target.id;
+  display.innerText += e.target.id;
+}
+
+equals.addEventListener("click", computeResult);
+
+function computeResult(e) {
+  e.preventDefault();
+
+    display.innerText = eval(display.innerText);
+    
+
+}
+
+clear.addEventListener("click", clearResult);
+
+function clearResult(e) {
+  e.preventDefault();
+
+  history.innerText = "";
+  display.innerText = "";
+}
+
+backspace.addEventListener("click", removeNumber);
+
+function removeNumber(e) {
+  e.preventDefault();
+
+  history.innerText = display.innerText.slice(0, -1);
+  display.innerText = display.innerText.slice(0, -1);
+}
